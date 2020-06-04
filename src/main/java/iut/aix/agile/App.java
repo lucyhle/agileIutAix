0452fd34429fe6aca1f137301b6ee5c456039207
Lucyhle <lucyhle@gmail.com>
2020-06-04 19:35:31 +0200

    first commit

---
+++ src/main/java/iut/aix/agile/App.java
@@ -0,0 +1,13 @@
+package iut.aix.agile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
